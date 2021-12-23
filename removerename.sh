ab15105a16034b5c03d75d2ec2f4aa6bb4173c3b
Pakobbix <Pakobbix@gmail.com>
2021-12-23 18:27:41 +0100

    Groß Kleinschreibung ignorieren. Auswahl zum löschen funktioniert auch mit teilübereinstimmung (Solange der Begriff nur einmal erwähnt wird. Möchte man Hawkeye entfernen, reicht es Haw einzugeben, da es keine weitere Überseintimmung gibt

--- removerename.sh
+++ removerename.sh
@@ -17,14 +17,13 @@ clear
 echo -e "Wähle das Keyword aus, für welchen eintrag du aus dem ${green}rename.sh Skript ${red}löschen${white} möchtest"
 echo "$entrys"
 read -rp "Anime/Serie zu löschen: " key1
-tvdbid=$(grep "$key1" "$rename" | grep "\-\-q" | sed 's/.*\-\-q //g' | cut -d" " -f1)
+tvdbid=$(grep -i "$key1" "$rename" | grep "\-\-q" | sed 's/.*\-\-q //g' | cut -d" " -f1)
 curl -sL https://www.thetvdb.com/dereferrer/series/"$tvdbid" | grep -i -A1 "deu" | grep "data-title" | sed 's/.*="\|"//g' | sed "s/&rsquo;/'/g"
 read -rp "$(echo -e Bist du sicher, dass du "${yellow}""$key1" "${red}"löschen"${white}" willst?) (y/N) " sure
-if [ "${sure,,}" == "y" ]
- then
-  grep -v "$key1" "$rename" > "$rename".bak
-  mv "$rename".bak "$rename"
-  chmod +x "$rename"
- else
-  exit
+if [ "${sure,,}" == "y" ]; then
+    grep -v "$key1" "$rename" >"$rename".bak
+    mv "$rename".bak "$rename"
+    chmod +x "$rename"
+else
+    exit
 fi
