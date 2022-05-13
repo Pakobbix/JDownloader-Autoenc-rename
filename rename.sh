a78d62eadbf9bafe219f65917a8788ac3f3dbc02
Pakobbix <Pakobbix@gmail.com>
2022-05-13 16:45:59 +0200

    Renaming ausgelagert.
    Das Umbenennen wurde ausgelagert auf eine extra Datei, die die Einträge beinhaltet. Dadurch werden selbst eingegebene Einträge nicht mehr überschrieben, sobald man die Skripte aktualisiert/neu runterlädt.

--- rename.sh
+++ rename.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 ## Farben für das Log
-
 red='\033[0;31m'
 green='\033[0;32m'
 white='\033[0;37m'
 yellow='\033[0;33m'
 purple='\033[0;35m' # ${purple}
+
+#Liste für die Umbenennung
+renamelist="$3"
 
 #Ordner Pfade
 
@@ -16,15 +18,29 @@ out="$2"
 
 log="$1"
 
+log_msg() {
+  echo -e "${yellow}$(date +"%d.%m.%y %T")${white}  $1${white}" >>"${log[@]}"
+}
+
+filebot_rename() {
+  filebot -rename "$v" --db "$1" -non-strict --lang German --format "/mnt/Medien/$2/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q "$3" >>"${log[@]}"
+}
+
+names() {
+  curl -sL https://www.thetvdb.com/dereferrer/series/"$1" | grep -i -A1 "deu" | grep "data-title" | sed 's/.*="\|"//g' | sed "s/&rsquo;/'/g"
+}
+
 ## Script start
 
-echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Starte ${green}rename.sh${white} Skript" >>"${log[@]}"
+log_msg "Starte ${green}rename.sh${white} Skript"
 
 if pgrep -f 'jdautoenc.sh' >/dev/null 2>&1; then # ignore
-  echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Warte auf das beenden vom Encoden" >>"${log[@]}"
+  log_msg "Warte auf das beenden vom Encoden"
+  sleep 2
 fi
 
 while pgrep -f 'jdautoenc.sh' >/dev/null 2>&1; do
+  log_msg "Warte 1 Minute vor dem nächsten Versuch"
   sleep 1m
 done
 
@@ -32,63 +48,51 @@ while pgrep -f 'startencode.sh' >/dev/null 2>&1; do
   sleep 1m
 done
 
-## Hier kommt ein neuer Loop für filebot
-## Bevor wir die Animes und Serien umbennen, überprüfen wir ob ffmpeg noch encoded.
-## Einfach nur um zu verhindern, dass filebot eine Datei umbennent, die noch encoded wird
-## und bei zuvielen Dateien mehrere filebot Prozesse versuchen die selbe Datei umzubenennen
-## (unwahrscheinlich aber lieber vorsichtig als nachher blöd dazustehen)
-
-sleep $(((RANDOM % 10) + 1))s
-
-for v in "${out[@]}"*.mkv; do
-  echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Starte die Umbennenung der encoded Videos" >>"${log[@]}"
-  if [ -z "$v" ]; then
-    echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Keine Video files zum Umbenennen" >>"${log[@]}"
-  elif [[ ${v,,} == *"insanity"* ]]; then                                                                                                                       # insanity
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 407682 >>"${log[@]}" # insanity
-  elif [[ ${v,,} == *"banished"*"party"* ]]; then                                                                                                               # banished party
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 400573 >>"${log[@]}" # banished party
-  elif [[ ${v,,} == *"mushoku"*"tensei"* ]]; then                                                                                                               # mushoku tensei
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 371310 >>"${log[@]}" # mushoku tensei
-  elif [[ ${v,,} == *"faraway"*"paladin"* ]]; then                                                                                                              # faraway paladin
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 401915 >>"${log[@]}" # faraway paladin
-  elif [[ ${v,,} == *"assassin"*"reincarnated"* ]]; then                                                                                                        # assassin reincarnated
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 398193 >>"${log[@]}" # assassin reincarnated
-  elif [[ ${v,,} == *"world"*"trigger"* ]]; then                                                                                                                # world trigger
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 283934 >>"${log[@]}" # world trigger
-  elif [[ ${v,,} == *"scarlet"*"nexus"* ]]; then                                                                                                                # scarlet nexus
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 399222 >>"${log[@]}" # scarlet nexus
-  elif [[ ${v,,} == *"shaman"*"king"* ]]; then                                                                                                                  # shaman king
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 383837 >>"${log[@]}" # shaman king
-  elif [[ ${v,,} == *"hawkeye"* ]]; then                                                                                                                        # Hawkeye
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Serien/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 367146 >>"${log[@]}" # Hawkeye
-  elif [[ ${v,,} == *"kurokos"*"basketball"* ]]; then                                                                                                           # kurokos basketball
-    filebot -rename "$v" --db TheTVDB -non-strict --lang German --format "/mnt/Medien/Animes/{n} ({y})/Season {s}/{n} - {s00e00} - {t}" --q 257765 >>"${log[@]}" # kurokos basketball
-  else
-    {
-      echo -e "${yellow}$(date +"%d.%m.%y %T")${white} ${red}Keine Übereinstimmung in den Vordefinierten Animes/Serien${white}"
-      echo -e "${yellow}$(date +"%d.%m.%y %T")${white} ${red}Um Fehler bei der Automatisierten Umbennenung zu verhindern, werden wenn nur Filme automatisch umbennant${white}"
-      echo -e "${yellow}$(date +"%d.%m.%y %T")${white} ${red}Überprüfe nun anhand der Länge des Videos ob es ein Film ist. Ansonsten Script beenden${white}"
-    } >>"${log[@]}"
-    duration=$(ffprobe -hide_banner -loglevel error -v quiet -stats -i "$v" -show_entries format=duration -v quiet -of csv="p=0" | sed -e 's/\..*//g')
-    if [ "$duration" -gt "4000" ]; then # ignore
-      echo -e "${yellow}$(date +"%d.%m.%y %T")${white} ${purple}$v${white} ist ein Film, extrahiere Namen für Ordner" >>"${log[@]}"
-      movie=$(basename "$v")
-      folder=$(basename "$v" .mkv)
-      ## Hier wird nun ein Ordner mit dem Namen des Videos erstellt. Dies ist eine Vorsichtsmaßnahme, da mein Ordner (encoded) auch bei eindeutigen Film
-      ## Namen immer wieder dazu führte, dass der Film als "ENCODED EXPLODED" umbenannt wurde -.-
-
-      echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Erstelle Ordner $folder" "$yellow" "$white" "$folder" >>"${log[@]}"
-      mkdir "$folder"
-      echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Verschiebe ${purple}$v${white} nach $folder" >>"${log[@]}"
-      mv "$v" "$out""$folder" >>"${log[@]}"
-      {
-        echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Fange an mit der Umbenennung"
-        filebot -rename "$out""$folder"/"$movie" --db TheMovieDB -non-strict --lang German --format "/mnt/Medien/Filme/{n} ({y})"
-        echo -e "${yellow}$(date +"%d.%m.%y %T")${white} Falls Umbennung erfolgreich,  ${red}Lösche${white} Ordner"
-      } >>"${log[@]}"
-      sleep 5s
-      rmdir "$folder"
-    fi
-  fi
+# Erstelle lock Datei, Warte 5 Sekunden und überprüfe nochmals
+echo $$ >/tmp/rename.lock
+sleep 1
+while [ -f rename.lock ]; do
+  log_msg "${red} Rename läufts bereits! Warte auf dessen durchlauf"
+  sleep 5
 done
+
+while read -r name; do
+  read -r keyword1
+  read -r keyword2
+  read -r format
+  read -r tvdbid
+  read -r nextentry
+
+  find -L "${out[@]}" -name '*.mkv' -or -name '*.mp4' | while IFS= read -r v; do
+
+    if [[ ${v,,} == *"$keyword1"*"$keyword2" ]]; then
+      log_msg "Bennene $v um in $name + Staffel + Episode + Episodentitel"
+      filebot_rename TheTVDB "$format" "$tvdbid"
+    else
+      log_msg "${red}Keine Übereinstimmung in den Vordefinierten Animes/Serien"
+      log_msg "${red}Um Fehler bei der Automatisierten Umbennenung zu verhindern, werden wenn nur Filme automatisch umbennant"
+      log_msg "${red}Überprüfe nun anhand der Länge des Videos ob es ein Film ist. Ansonsten Script beenden"
+      duration=$(ffprobe -hide_banner -loglevel error -v quiet -stats -i "$v" -show_entries format=duration -v quiet -of csv="p=0" | sed -e 's/\..*//g')
+      if [ "$duration" -gt "4000" ]; then # ignore
+        log_msg "${purple}$v${white} ist ein Film, extrahiere Namen für Ordner"
+        movie=$(basename "$v")
+        folder=$(basename "$v" .mkv)
+        ## Hier wird nun ein Ordner mit dem Namen des Videos erstellt. Dies ist eine Vorsichtsmaßnahme, da mein Ordner (encoded) auch bei eindeutigen Film
+        ## Namen immer wieder dazu führte, dass der Film als "ENCODED EXPLODED" umbenannt wurde -.-
+
+        log_msg "Erstelle Ordner $folder" "$yellow" "$white" "$folder"
+        mkdir "$out""$folder"
+        log_msg "Verschiebe ${purple}$v${white} nach $folder"
+        mv "$v" "$out""$folder" >>"${log[@]}"
+        log_msg "Fange an mit der Umbenennung"
+        filebot -rename "$out""$folder"/"$movie" --db TheMovieDB -non-strict --lang German --format "/mnt/Medien/Filme/{n} ({y})" >>"${log[@]}"
+        log_msg "Falls Umbennung erfolgreich, ${red}Lösche${white} Ordner"
+        sleep 5s
+        rmdir "$out"*
+      fi
+    fi
+
+  done
+done <"$renamelist"
+
+rm -f /tmp/rename.lock
