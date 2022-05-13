d8fc0b407d874344f76e8d0a00b4b4c7899dc3ef
Pakobbix <Pakobbix@gmail.com>
2022-05-13 17:44:52 +0200

    Anpassung an Umbenennungsliste
    Einträge werden aus der Liste und nicht mehr aus der rename.sh rausgesucht.
    Failsafe falls kein deutscher Titel bei tvdb vorhanden.

--- removerename.sh
+++ removerename.sh
@@ -8,34 +8,31 @@ yellow='\033[0;33m'
 
 # Rename Skript:
 
-rename=~/.local/scripts/rename.sh
+rename=~/.local/scripts/renamelist
 
 clear
 IFS=$'\n'
 frage="Wähle den zu löschenden Eintrag aus: "
-entrys=($(grep "\-\-q" "$rename" | sed 's/.* # //g'))
+entrys=($(awk 'NR % 6 == 1' "$rename" | sed 's/#.*/Beispiel Eintrag/g'))
 
 PS3="$frage "
 select entry in "${entrys[@]}" "Beenden"; do
-    if ((REPLY == 1 + ${#entrys[@]})); then
-        exit
-        break
+  if ((REPLY == 1 + ${#entrys[@]})); then
+    exit
+    break
 
-    elif ((REPLY > 0 && REPLY <= ${#entrys[@]})); then
-        tvdbid=$(grep -i "$entry" "$rename" | grep "\-\-q" | sed 's/.*\-\-q //g' | cut -d" " -f1)
-        curl -sL https://www.thetvdb.com/dereferrer/series/"$tvdbid" | grep -i -A1 "deu" | grep "data-title" | sed 's/.*="\|"//g' | sed "s/&rsquo;/'/g"
-        echo ""
-        read -rp "$(echo -e Bist du sicher, dass du "${yellow}""$entry" "${red}"löschen"${white}" willst?) (y/N) " sure
-        if [ "${sure,,}" == "y" ]; then
-            grep -v "$entry" "$rename" >"$rename".bak
-            mv "$rename".bak "$rename"
-            chmod +x "$rename"
-            break
-        fi
-        /bin/bash "${BASH_SOURCE[0]}"
-    else
-        echo "Ungültige Eingabe."
+  elif ((REPLY > 0 && REPLY <= ${#entrys[@]})); then
+    tvdbid=$(grep -A5 "$entry" "$renamelist" | awk 'NR % 5 == 0')
+    if [[ -z $(curl -sL https://www.thetvdb.com/dereferrer/series/"$tvdbid" | grep -i -A1 "deu" | grep "data-title" | sed 's/.*="\|"//g' | sed "s/&rsquo;/'/g") ]]; then
+      curl -sL https://www.thetvdb.com/dereferrer/series/"$tvdbid" | grep -i -A1 "en" | grep "data-title" | sed 's/.*="\|"//g' | sed "s/&rsquo;/'/g"
     fi
+    echo ""
+    read -rp "$(echo -e Bist du sicher, dass du "${yellow}""$entry" "${red}"löschen"${white}" willst?) (y/N) " sure
+    if [ "${sure,,}" == "y" ]; then
+      sed -i "/$entry/,+5d" "$renamelist"
+    fi
+    /bin/bash "${BASH_SOURCE[0]}"
+  else
+    echo "Ungültige Eingabe."
+  fi
 done
-
-xdg-open >/dev/null 2>&1 "https://www.anisearch.de/anime/index?text=banished+&quick-search=&char=all&q=true"
