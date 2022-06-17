19f5088521d7537478a60a82b9194b3db8e328ee
Pakobbix <Pakobbix@gmail.com>
2022-06-17 10:35:23 +0200

    switched failsafe language from multilanguage to main branch

--- addrename.sh
+++ addrename.sh
@@ -28,7 +28,7 @@ text_lang() {
   if [ -f "$language_Folder"/"$language"/addrename.lang ]; then
     grep "$1" "$language_Folder"/"$language"/addrename.lang | sed 's/^....//'
   else
-    curl -s https://raw.githubusercontent.com/Pakobbix/JDownloader-Autoenc-rename/Multilanguage/lang/en_US/addrename.lang | grep "$1" | sed 's/^....//'
+    curl -s https://raw.githubusercontent.com/Pakobbix/JDownloader-Autoenc-rename/main/lang/en_US/addrename.lang | grep "$1" | sed 's/^....//'
   fi
 }
 
